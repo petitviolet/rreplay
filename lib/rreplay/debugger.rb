dd603b9a80ecb559c214dbe3af676aeed93e53ac
petitviolet <violethero0820@gmail.com>
2019-12-31 22:29:36 +0900

    Enabled to output JSON debugging

--- lib/rreplay/debugger.rb
+++ lib/rreplay/debugger.rb
@@ -8,7 +8,15 @@ module Rreplay
     def out(&block)
       return unless @debug
 
-      @logger.write("#{Time.now.iso8601} - #{block.call}\n")
+      msg = block.call.then do |msg|
+        case msg
+        when Hash
+          msg.merge({ time: Time.now.iso8601 }).to_json
+        else
+          "#{Time.now.iso8601} - #{block.call}"
+        end
+      end
+      @logger.write("#{msg}\n")
     end
   end
 end
